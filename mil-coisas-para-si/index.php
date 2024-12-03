a9c0f9289004708fbdcfd3afc137049a62b10123
Toito3 <kitumbaantonio3@gmail.com>
2024-12-03 18:06:26 +0100

    Add files via upload

---
+++ mil-coisas-para-si/index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asserts/css/style.css">
+    <title>MIL COISAS PARA SI</title>
+</head>
+<body>
+<header>
+        <nav class="nav-bar">
+            <div class="logo">
+                <h1>AM SHOP</h1>
+             
+            </div>
+            <div class="nav-list">
+                <ul>
+                    <li class="nav-item"><a href="#" class="nav-link">Inicio</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Seviços</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Sobre</a></li>
+                </ul>
+            </div>
+            <div class="login-button">
+                <button></button>
+            </div>
+        </nav>
+    </header>
+    <div class="bloco">
+       <div class="img-1"> 
+       <img src="asserts/imgem/OIP.jfif" alt="" class="img1"> 
+
+       <h1>Pulseira feminina</h1>
+       <p>Transmite harmonia e bastante amor.</p>
+       <p>Preço:12.000 KZ </p>
+       <button class="adicionar">Adicionar</button>
+       </div>
+
+      
+
+       <div class="img-1"> <img src="asserts/imgem/OIP.jfif" alt="" class="img1"> 
+
+       <h1>Pulseira feminina</h1>
+       <p>Transmite harmonia e bastante amor.</p>
+       <p>Preço:12.000 KZ </p>
+       <button class="adicionar">Adicionar</button>
+       </div>
+
+
+       <div class="img-1"> <img src="asserts/imgem/OIP.jfif" alt="" class="img1"> 
+
+       <h1>Pulseira feminina</h1>
+       <p>Transmite harmonia e bastante amor.</p>
+       <p>Preço:12.000 KZ </p>
+       <button class="adicionar">Adicionar</button>
+       </div>
+
+
+       <div class="img-1"> <img src="asserts/imgem/OIP.jfif" alt="" class="img1"> 
+
+       <h1>Pulseira feminina</h1>
+       <p>Transmite harmonia e bastante amor.</p>
+       <p>Preço:12.000 KZ </p>
+       <button class="adicionar">Adicionar</button>
+       </div>
+    </div>
+    
+
+
+
+
+    <div class="bloco">
+       <div class="img-1"> 
+       <img src="asserts/imgem/OIP.jfif" alt="" class="img1"> 
+
+       <h1>Pulseira feminina</h1>
+       <p>Transmite harmonia e bastante amor.</p>
+       <p>Preço:12.000 KZ </p>
+       <button class="adicionar">Adicionar</button>
+       </div>
+
+      
+
+       <div class="img-1"> <img src="asserts/imgem/OIP.jfif" alt="" class="img1"> 
+
+       <h1>Pulseira feminina</h1>
+       <p>Transmite harmonia e bastante amor.</p>
+       <p>Preço:12.000 KZ </p>
+       <button class="adicionar">Adicionar</button>
+       </div>
+
+
+       <div class="img-1"> <img src="asserts/imgem/OIP.jfif" alt="" class="img1"> 
+
+       <h1>Pulseira feminina</h1>
+       <p>Transmite harmonia e bastante amor.</p>
+       <p>Preço:12.000 KZ </p>
+       <button class="adicionar">Adicionar</button>
+       </div>
+
+
+       <div class="img-1"> <img src="asserts/imgem/OIP.jfif" alt="" class="img1"> 
+
+       <h1>Pulseira feminina</h1>
+       <p>Transmite harmonia e bastante amor.</p>
+       <p>Preço:12.000 KZ </p>
+       <button class="adicionar">Adicionar</button>
+       </div>
+    </div>
+</body>
+</html>
